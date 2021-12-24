ff21e99665785f8f306f7616e86091feec1ea771
Chizaram Nwazuo <chizonwazuo@yahoo.com>
2021-12-24 12:38:50 +0100

    fix netlify FOUC bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,4 @@
  */
 
 // You can delete this file if you're not using it
-import("./src/styles/globals.css")
+// import("./src/styles/globals.css")
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../styles/globals.css"
 
 import Seo from "../components/seo"
 import Navbar from "../components/Navbar"
